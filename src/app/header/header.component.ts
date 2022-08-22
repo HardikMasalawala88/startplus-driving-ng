f8595915fb3cb458e679143af747b37b68bcb58f
Hardikmasalawala88 <Hardikmasalawala88@gmail.com>
2022-08-22 20:38:45 +0530

    New UI updated with chnages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  navbarfixed:boolean=false;
+  @HostListener('window:scroll',['$event'])
+  onscroll(){
+    if(window.scrollY>100){
+      this.navbarfixed=true;
+    }
+    else{
+      this.navbarfixed=false;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
